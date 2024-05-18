34dce8f68d6c60e4a4624aeb801f3240d399e44b
Jie Chen <42862243+jiechencn@users.noreply.github.com>
2024-05-18 17:49:03 +0800

    Update devide_fish.py

--- Level0/devide_fish.py
+++ Level0/devide_fish.py
@@ -1,3 +1,6 @@
+
+# 5个人捕鱼后分鱼，第一个人将鱼分成5分，把多余的一条鱼放掉，拿走自己的一份；第二个人也将鱼分成5份，把多余的一条鱼放掉，拿走自己的一份；其他3个人也按照同样的方法那鱼，问他们至少捕到多少条鱼？
+
 def devide_fish(people_count):
     fish = people_count + 1
     while True:
@@ -6,9 +9,11 @@ def devide_fish(people_count):
         for i in range(people_count):
             if (total-1) % people_count == 0:
                 total = (total-1)/people_count * (people_count - 1)
+                # 当前总数是，扔掉一条后平均分成5份，又被拿走一份
             else:
                 is_enough = False
                 fish += people_count * 1
+                # 当前不够分，每人再多一条
                 break
 
         if is_enough:
